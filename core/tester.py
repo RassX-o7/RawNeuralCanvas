58c79f7dc0de4b17cd92f6e82e3c6f228fc9f301
RassX-o7 <rassx777@gmail.com>
2026-06-10 15:22:35 +0530

    fix testing for batch size 1

--- core/tester.py
+++ core/tester.py
@@ -22,10 +22,10 @@ class Tester:
             confidence=self.NN.model_activations[-1][prediction]
             self.true_labels[int(true_label)]=self.true_labels.get(int(true_label),0) +1
             self.pred_labels[int(prediction)]=self.pred_labels.get(int(prediction),0) +1
+            # print(true_label, type(true_label), np.shape(true_label))
 
-            self.cm_true[true_label][prediction]+=1
-            self.cm_ideal[true_label][true_label]+=1
-
+            self.cm_true[true_label.item()][prediction]+=1 # imp since after .get() vectorized batch size 1 returns 1d array 
+            self.cm_ideal[true_label.item()][true_label]+=1
             if self.visulaizer is True:
                 plt.imshow(test_image,cmap="gray")
                 plt.title(f"Correct Label is {true_label} and predicted label is {prediction} confidence is {confidence*100}%")
